0155da9834b296d3b1acba91f91a70aafe28c646
BOB SUN <2428059449@qq.com>
2025-08-26 11:00:05 -0500

    完成day2-(08)完成前后端联调

--- xuecheng-plus/xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
+++ xuecheng-plus/xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
@@ -4,8 +4,10 @@ import com.xuecheng.base.model.PageParams;
 import com.xuecheng.base.model.PageResult;
 import com.xuecheng.content.model.dto.QueryCourseParamsDto;
 import com.xuecheng.content.model.po.CourseBase;
+import com.xuecheng.content.service.CourseBaseInfoService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,16 +20,13 @@ import java.util.List;
 @Api(value = "课程信息编辑接口",tags = "课程信息编辑接口")
 public class CourseBaseInfoController {
 
+    @Autowired
+    CourseBaseInfoService courseBaseInfoService;
+
     @ApiOperation("课程查询接口")
     @PostMapping("/course/list")
-    public PageResult<CourseBase> list(PageParams pageParams, @RequestBody(required = false) QueryCourseParamsDto queryCourseParams){
-        CourseBase courseBase = new CourseBase();
-        courseBase.setName("测试名称");
-        courseBase.setCreateDate(LocalDateTime.now());
-        List<CourseBase> courseBases = new ArrayList<CourseBase>();
-        courseBases.add(courseBase);
-        PageResult<CourseBase> pageResult = new PageResult<>(courseBases, 10, 1, 10);
-        return pageResult;
+    public PageResult<CourseBase> list(PageParams pageParams, @RequestBody(required = false) QueryCourseParamsDto queryCourseParamsDto){
+        return courseBaseInfoService.queryCourseBaseList(pageParams, queryCourseParamsDto);
 
     }
 
