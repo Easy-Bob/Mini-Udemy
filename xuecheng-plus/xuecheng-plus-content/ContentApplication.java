f0a61ba8a3b300eed1fb94ef25d5d7a3a8569016
BOB SUN <2428059449@qq.com>
2025-08-25 23:10:15 -0500

    完成day1; 完成day2-(1)DAO接口查询测试

---
+++ xuecheng-plus/xuecheng-plus-content/ContentApplication.java
@@ -0,0 +1,9 @@
+package com.xuecheng;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentApplication {
+    public static void main(String[] args) {
+
+    }
+}
