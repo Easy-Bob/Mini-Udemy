957ee146bb579aa8f8c3507652def6ba742ba624
BOB SUN <2428059449@qq.com>
2025-08-27 17:52:25 -0500

    完成nacos对api-content和api-service的注册

--- xuecheng-plus/xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/ContentApiApplication.java
+++ xuecheng-plus/xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/ContentApiApplication.java
@@ -2,10 +2,12 @@ package com.xuecheng;
 import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableSwagger2Doc
+@EnableDiscoveryClient
 public class ContentApiApplication {
 
     public static void main(String[] args) {
