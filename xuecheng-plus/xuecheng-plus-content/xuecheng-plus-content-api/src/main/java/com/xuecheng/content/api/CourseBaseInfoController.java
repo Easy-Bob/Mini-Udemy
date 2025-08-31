a5599d45e1d5eac9eec7b23d8371586b04c5bb21
BOB SUN <2428059449@qq.com>
2025-08-31 17:17:36 -0500

    SpringSecurity + JWT

--- xuecheng-plus/xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
+++ xuecheng-plus/xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
@@ -12,8 +12,10 @@ import com.xuecheng.content.service.CourseBaseInfoService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import com.xuecheng.util.SecurityUtil;
 
 import javax.validation.Valid;
 import javax.validation.Validation;
@@ -45,6 +47,11 @@ public class CourseBaseInfoController {
     @ApiOperation("根据id查询课程信息")
     @GetMapping("/course/{courseId}")
     public CourseBaseInfoDto getCourseBaseById(@PathVariable Long courseId) {
+        // 获取当前用户信息
+//        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//        System.out.println(principal);
+        SecurityUtil.XcUser user = SecurityUtil.getUser();
+        System.out.println(user.getName());
         return courseBaseInfoService.getCourseBaseById(courseId);
     }
 
