d55ea8127ffcf55a445be7f53bd088ab10a41ec2
BOB SUN <2428059449@qq.com>
2025-09-02 23:31:14 -0500

    complete course elasticsearch

--- xuecheng-plus/xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/ContentApiApplication.java
+++ xuecheng-plus/xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/ContentApiApplication.java
@@ -3,11 +3,13 @@ import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableSwagger2Doc
 @EnableDiscoveryClient
+@EnableFeignClients(basePackages = {"com.xuecheng.content.feignclient"})
 public class ContentApiApplication {
 
     public static void main(String[] args) {
